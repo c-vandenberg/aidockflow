391b2c7712c57c6db2b21bbe855f5bc5d5756374
Chris van den Berg <cvandenberg1105@googlemail.com>
2025-04-10 13:29:41 +0100

    Further fixes for API call logic

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,14 @@
+import os
 from typing import List
 
+from data.utils.logging_utils import configure_logger
 from data.ingestion.bioactive_compounds_extraction import ChEMBLExtractor, PubChemExtractor
 
 def main():
-    chembl_extractor = ChEMBLExtractor()
-    pubchem_extractor = PubChemExtractor()
+    os.makedirs('../var/log', exist_ok=True)
+    logger = configure_logger('../var/log/data_ingestion.log')
+    chembl_extractor = ChEMBLExtractor(bioactivity_threshold=1000, logger=logger)
+    pubchem_extractor = PubChemExtractor(bioactivity_threshold=1000, logger=logger)
 
 
     pubchem_bioacties: List[str] = pubchem_extractor.get_bioactive_compounds(target_uniprot_id='P00533')
--- src/data/ingestion/bioactive_compounds_extraction.py
+++ src/data/ingestion/bioactive_compounds_extraction.py
@@ -1,12 +1,22 @@
+import time
 import requests
+import logging
+import concurrent.futures
 from abc import ABC, abstractmethod
+from functools import partial
 from typing import List, Optional
 
 from chembl_webresource_client.new_client import new_client
 import pubchempy as pcp
+from click.core import batch
 from pubchempy import request
 
-from src.data.utils.mappings import uniprot_to_gene_id_mapping
+from data.utils.logging_utils import configure_logger
+from data.utils.mappings import uniprot_to_gene_id_mapping
+from data.utils.pubchem_api import (get_active_aids, get_active_cids,
+                                    get_active_cids_wrapper, get_compounds_in_batches,
+                                    batch_iterable, get_compound_potency)
+from rdkit.RDLogger import logger
 
 
 class BioactivityExtractorInterface(ABC):
@@ -59,8 +69,9 @@ class ChEMBLExtractor(BioactivityExtractorInterface):
     def __init__(
         self,
         client = None,
-        bioactivity_threshold: float = 1000, # for compounds with IC50 <= 1 µM
-        bioactivity_measure: str = 'IC50'
+        bioactivity_threshold: Optional[float] = None, # In nM (e.g. 1000 nM threshold to filter for compounds with IC50 <= 1 µM)
+        bioactivity_measure: str = 'IC50',
+        logger: Optional[logging.Logger] = None
     ):
         if client is None:
             self._client = new_client
@@ -68,7 +79,7 @@ class ChEMBLExtractor(BioactivityExtractorInterface):
             self._client = client
         self._bioactivity_threshold = bioactivity_threshold
         self._bioactivity_measure = bioactivity_measure
-
+        self._logger = logger if logger else logging.getLogger(__name__)
 
     def get_bioactive_compounds(self, target_uniprot_id: str) -> List[str]:
         """
@@ -95,6 +106,7 @@ class ChEMBLExtractor(BioactivityExtractorInterface):
         target_results = self._client.target.filter(target_components__accession=target_uniprot_id)
         if not target_results:
             raise ValueError(f"No matching target found for UniProt ID {target_uniprot_id}")
+
         target_data = target_results[0]
         target_id = target_data['target_chembl_id']
 
@@ -107,12 +119,17 @@ class ChEMBLExtractor(BioactivityExtractorInterface):
         for compound in bioactive_compounds:
             compound_smiles: str = compound.get('canonical_smiles')
 
+            if not compound_smiles:
+                continue
+
             try:
                 bioactivity_value = float(compound.get('standard_value', 0))
             except (ValueError, TypeError):
-                bioactivity_value = None
+                continue
 
-            if compound_smiles and bioactivity_value is not None and bioactivity_value <= self._bioactivity_threshold:
+            if not self._bioactivity_threshold:
+                bioactive_smiles.append(compound_smiles)
+            elif self._bioactivity_threshold and bioactivity_value <= self._bioactivity_threshold:
                 bioactive_smiles.append(compound_smiles)
 
         return bioactive_smiles
@@ -138,14 +155,24 @@ class PubChemExtractor(BioactivityExtractorInterface):
     """
     def __init__(
         self,
-        bioactivity_threshold: float = 1000,  # for compounds with IC50 <= 1 µM
+        bioactivity_threshold: Optional[float] = None, # In nM (e.g. 1000 nM threshold to filter for compounds with IC50 <= 1 µM)
+        logger: Optional[logging.Logger] = None
     ):
         self._bioactivity_threshold = bioactivity_threshold
+        self._logger = logger if logger else logging.getLogger(__name__)
 
     def get_bioactive_compounds(self, target_uniprot_id: str) -> List[str]:
         """
         Retrieve canonical SMILES for compounds for a given target from PubChem.
         The target is provided as a UniProt accession (e.g. "P00533").
+
+        This method performs the following steps:
+
+        1. Maps the UniProt accession to an NCBI GeneID.
+        2. Uses the GeneID to query PubChem’s BioAssay API for assay IDs (AIDs).
+        3. For each assay, extracts the active compound IDs (CIDs).
+        4. Retrieves compound details (including canonical SMILES) for the aggregated CIDs.
+        5. Optionally filters compounds based on potency.
 
         Parameters
         ----------
@@ -155,28 +182,104 @@ class PubChemExtractor(BioactivityExtractorInterface):
         Returns
         -------
         List[str]
-            A list of canonical SMILES strings for compounds matching the target UniProt accession.
+            A list of canonical SMILES strings for compounds matching the target criteria.
         """
+        # 1) Map the UniProt accession to an NCBI GeneID.
         target_gene_id = self._lookup_target_gene_id(target_uniprot_id)
         if not target_gene_id:
-            print(f"Could not determine GeneID for target '{target_uniprot_id}'.")
+            self._logger.error(f"Could not determine GeneID for target '{target_uniprot_id}'.")
             return []
 
-        bioactive_compounds = pcp.get_compounds(target_gene_id, 'name')
-        bioactive_smiles: List = []
+        # 2) Query the BioAssay API to get assay IDs (AIDs) for the target GeneID.
+        try:
+            aid_list = get_active_aids(target_gene_id)
+        except Exception as e:
+            self._logger.error(f"Error retrieving assay IDs for GeneID {target_gene_id}: {e}")
+            return []
 
-        for compound in bioactive_compounds:
-            compound_smiles = compound.canonical_smiles
-            if not compound_smiles:
-                continue
+        # 3) For each assay, retrieve active compound IDs (CIDs) and aggregate them.
+        #    Create thread pool using Python’s `ThreadPoolExecutor` to issue multiple API calls concurrently in batches
+        cids_api_start: float = time.time()
+        active_cids = set()
 
-            compound_potency = self._get_compound_potency(compound=compound, target_gene_id=target_gene_id)
-            if compound_potency is None or compound_potency > self._bioactivity_threshold:
-                continue
+        # Create a new partial function with `logger` argument fixed. This allows us to pass a fixed `logger` argument
+        # to the `get_active_cids_wrapper()` function when it is mapped to each AID element in `aid_list` via
+        # `concurrent.futures.ThreadPoolExecutor.map()`
+        get_active_cids_wrapper_partial = partial(get_active_cids_wrapper, logger=self._logger)
 
-            bioactive_smiles.append(compound_smiles)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=9) as executor:
+            # Map and apply partial function of `cids_for_aid_wrapper()` to every element in `aid_list` concurrently
+            results = list(executor.map(get_active_cids_wrapper_partial, aid_list))
 
-        return bioactive_smiles
+            for cids in results:
+                active_cids.update(cids)
+
+        cids_api_end: float = time.time()
+        self._logger.info(f'PubChem CID Total API Query Time: {round(cids_api_end - cids_api_start)} seconds')
+
+        if not active_cids:
+            self._logger.error(f"No active compounds found for GeneID {target_gene_id}.")
+            return []
+
+        # 4) Retrieve compound details using PubChemPy with the aggregated CIDs.
+        bioactive_compound_api_start: float = time.time()
+        bioactive_compounds = get_compounds_in_batches(cids=list(active_cids), logger=self._logger)
+        bioactive_compound_api_end: float = time.time()
+        self._logger.info(f'PubChem Bioactive Compounds From CIDs Total API Query Time: '
+                          f'{round(bioactive_compound_api_end - bioactive_compound_api_start)} seconds')
+
+        bioactive_smiles: List[str] = []
+        if not self._bioactivity_threshold:
+            for compound in bioactive_compounds:
+                compound_smiles = compound.canonical_smiles
+                if not compound_smiles:
+                    continue
+
+                bioactive_smiles.append(compound_smiles)
+
+            return bioactive_smiles
+        else:
+            compound_potency_api_start: float = time.time()
+
+            # Create a new partial function with `target_gene_id` and `logger` argument fixed. As before, this allows
+            # us to pass these fixed arguments to `self._get_bioactive_compound_potency()` when it is mapped to each
+            # compound element in the batched `bioactive_compounds` iterable via
+            # `concurrent.futures.ThreadPoolExecutor.map()`
+            get_bioactive_compound_potency_partial = partial(
+                self._get_bioactive_compound_potency,
+                target_gene_id=target_gene_id,
+                logger=self._logger
+            )
+            for compound_batch in batch_iterable(iterable=bioactive_compounds):
+                # Process the current `bioactive_compounds` batch concurrently using a thread pool
+                with concurrent.futures.ThreadPoolExecutor(max_workers=9) as executor:
+                    # Map and apply partial function of `self._get_bioactive_compound_potency()` to every element in
+                    # current `bioactive_compounds` batch concurrently
+                    potencies = list(
+                        executor.map(
+                            get_bioactive_compound_potency_partial,
+                            compound_batch
+                        )
+                    )
+
+                # Iterate over compounds in the batch alongside their potencies
+                for compound, potency in zip(compound_batch, potencies):
+                    compound_smiles = compound.canonical_smiles
+                    if not compound_smiles:
+                        continue
+
+                    # Filter by potency if a potency is provided.
+                    if potency is None or potency > self._bioactivity_threshold:
+                        continue
+
+                    bioactive_smiles.append(compound_smiles)
+
+            compound_potency_api_end: float = time.time()
+            self._logger.info(f'PubChem Bioactive Compound Potencies Total API Query Time: '
+                              f'{round(compound_potency_api_end - compound_potency_api_start)} seconds')
+
+            return bioactive_smiles
+
 
     @staticmethod
     def _lookup_target_gene_id(target: str) -> Optional[str]:
@@ -195,6 +298,14 @@ class PubChemExtractor(BioactivityExtractorInterface):
             The corresponding NCBI GeneID if found, otherwise None.
         """
         return uniprot_to_gene_id_mapping(target)
+
+    @staticmethod
+    def _get_bioactive_compound_potency(
+        compound: pcp.Compound,
+        target_gene_id: str,
+        logger: logging.Logger = None
+    ) -> Optional[float]:
+        return get_compound_potency(compound=compound, target_gene_id=target_gene_id)
 
 
     @staticmethod
